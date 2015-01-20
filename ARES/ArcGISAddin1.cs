cf1fa9f42d8fafdb0aed2b61c6a9bc5c66e9c64a
JoeYu <njkon@foxmail.com>
2015-01-20 11:59:48 -0500

    Added some test code

---
+++ ARES/ArcGISAddin1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using ARES.Editor;
+
+namespace ARES
+{
+    public class ArcGISAddin1 : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ArcGISAddin1()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
